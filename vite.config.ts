4628369a097aba19f2e56ec8d6eee117a22b5973
rohand575 <rohan.dhanawade97@gmail.com>
2025-10-01 15:54:19 +0200

    build: output to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  build: { outDir: 'docs' },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
